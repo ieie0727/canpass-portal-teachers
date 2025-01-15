b81f8ad453aaca735dd2a5d1df0d23ad6a184c7e
“ieie0727” <you.inoue.727@gmail.com>
2025-01-15 09:08:50 +0900

    viewの微修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,7 +38,7 @@
                         <div class="card-body">
                             <h5 class="card-title">テスト管理</h5>
                             <p class="card-text">教科ごとの小テスト作成・管理を行います。</p>
-                            <a href="{{ route('sections.home') }}" class="btn btn-outline-primary">学習管理へ</a>
+                            <a href="{{ route('sections.home') }}" class="btn btn-outline-primary">テスト管理へ</a>
                         </div>
                     </div>
                 </div>
